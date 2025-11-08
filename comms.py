dec391ecbe7bbb028e07e0ebec95293431dea552
PaulosK <52920283+PaulosKapa@users.noreply.github.com>
2025-11-08 16:04:15 +0200

    Update comms.py

--- comms.py
+++ comms.py
@@ -1,16 +1,20 @@
 import socket
 import serial
+#start serial connection
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5)
 print("Waiting for IP...")
 while True:
     if ser.in_waiting > 0:
+		#get the ip from godot
         ip = ser.readline().decode('utf-8').strip()
         HOST = ip
         break
 print(HOST)
+#set the port
 PORT = 65432
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
+#print the data that get sent from raspberry pi
 try:
 	while True:
 		data = client_socket.recv(1024).decode()
