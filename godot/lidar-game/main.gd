39ac86c069bf33786902dee7aed0e8b86d109e79
PaulosK <52920283+PaulosKapa@users.noreply.github.com>
2025-11-08 15:41:08 +0200

    automatic connection
    Added the option to connect with the raspberry pi automatically without typing the ip manually (godot side code)

--- godot/lidar-game/main.gd
+++ godot/lidar-game/main.gd
@@ -1,32 +1,41 @@
 extends Node3D
-
+#start a new tcp server
 var server := TCPServer.new()
 var client : StreamPeerTCP
-
 const PORT = 65432
+#boolean variable for the movement of the node
 var movement = true
+#get the raycast node
 @onready var angle = get_node("Camera3D/RayCast3D")
+#boolean for sending data to the raspi
 var send = false
 signal connected
-var start = false
+#get the c# node
+@onready var csharp = get_node("Node")
+#set false for production
+var start = true
 
 func _ready():
-	var err = server.listen(PORT)
-	if err != OK:
-		print("Server failed to start: ", err)
-	else:
-		print("Server listening on port ", PORT)
+	#only if the ip was sent to the raspberry
+	if (csharp.ipSent == true):
+		#listen to the port and check if it can start
+		var err = server.listen(PORT)
+		if err != OK:
+			print("Server failed to start: ", err)
+		else:
+			print("Server listening on port ", PORT)
 
 
 func _process(_delta):
 	# Check for new connections
-	if server.is_connection_available():
+	if csharp.ipSent == true and server.is_connection_available():
 		client = server.take_connection()
 		print("Client connected!")
 		emit_signal("connected")
 		start = true
-	# If connected, send data
+	# If connected, send data and send = true
 	if client and client.get_status() == StreamPeerTCP.STATUS_CONNECTED and send == true:
+		#send angles that the raycast gets
 		var message = str(angle.get_angles())
 		client.put_data(message.to_utf8_buffer())
 		
@@ -34,15 +43,19 @@ func _process(_delta):
 func _physics_process(delta):
 	if(start == true):
 		moving()
+		
 func _on_ray_cast_3d_stop_moving() -> void:
+	#when the raycast detects and obstacle, send the data to the raspberry pi and stop moving
 	movement = false
 	send = true
 	
 func moving():
+	#simple movement script
 	if(movement):
 		translate(Vector3(0,0,-.01))
 
 
 func _on_ray_cast_3d_start_moving() -> void:
+	#stop sending data and start moving
 	movement = true
 	send = false
--- godot/lidar-game/ray_cast_3d.gd
+++ godot/lidar-game/ray_cast_3d.gd
@@ -1,37 +1,48 @@
 extends RayCast3D
-
+#how fast the raycast rotates
 var rotation_speed = 90.0 # degrees per second
+#direction of the raycast
 var direction = 1 # 1 for increasing, -1 for decreasing
+#a list for inserting the obstacles
 var items = []
+#get the camera node 
 @onready var camera = get_parent()
 @onready var ray = $'.'
+#signals for when and obstacle is detected
 signal stop_moving
 signal start_moving
 var prev_angle = 0
 var i = 0
 var angles_to_send = 0
 var previous_angle = 0
-var start_game = false
+#set false for production
+var start_game = true
+#set rotation degrees to the left angle
 func _ready():
 	rotation_degrees.y += 60
+#
 func _process(delta):
+	#if the game has start, rotate the raycast
 	if start_game == true:
 		rot_func(60.0, delta, ray)
-		var deg = int(rotation_degrees.y)
+		#get the current angle
 		var current_angle = rotation_degrees.y
+		#the code divides the space on 20 dividents. The array that will contain the 0 and 1 for the obstacles 
+		#needs this in order to move the index
+		#this line of code checks if the rotation has change
 		if int(current_angle / 6) != int(prev_angle / 6):
 			i += 1
-				
-		
+		#when the ray collides stop moving and enter 1 if the collider is box otherwise enter 0
 		if ray.is_colliding():
 			emit_signal("stop_moving")
 			var collider = ray.get_collider()
 			if collider.is_in_group("box"):
-			#	print("Hit object:", collider)
 				if int(current_angle / 6) != int(prev_angle / 6):
 					items.append(1)
+				#change the rotation of the servo (based on angles to send). If the obstacle is on the 
+				#left, move the camera on the right
+				#should probably make a smoother transition based on the index of the obstacle
 				if(len(items)>1):
-					
 					if(items[i-1] == 0):
 						if(i<10):
 							get_parent().get_parent().rotation_degrees.y += 6
@@ -40,23 +51,18 @@ func _process(delta):
 						else:
 							get_parent().get_parent().rotation_degrees.y -= 6
 							angles_to_send = (rotation_degrees.y - 6)
-					#print(int(angles_to_send))
-				
 					
 		else:
 				if int(current_angle / 6) != int(prev_angle / 6):
 					items.append(0)
 					emit_signal("start_moving")
-					
+	#clear the array and set the index to 0
 		if(i>19):
 			items.clear()
 			i = 0
 		prev_angle = current_angle
-		#else:
-			#get_parent().get_parent().position.x = 10
-			
 
-
+#rotate the raycast from 60 to -60 degrees
 func rot_func(limit, delta, target):
 	target.rotation_degrees.y += rotation_speed * delta * direction
 	# When reaching limits, flip direction
@@ -66,12 +72,13 @@ func rot_func(limit, delta, target):
 	elif target.rotation_degrees.y <= -limit:
 		target.rotation_degrees.y = -limit
 		direction = 1
+#send only the angles that don't match the previous angles (in case te raycast gets stuck on an object)
 func get_angles():
 	if (previous_angle!=angles_to_send):
 		previous_angle = angles_to_send
 		return(int(angles_to_send))
 	return("e")
 
-
+#for starting the game
 func _on_node_3d_connected() -> void:
 	start_game = true
