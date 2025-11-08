dfede464644201dbe8dccbdd4cc359f57e874417
PaulosK <52920283+PaulosKapa@users.noreply.github.com>
2025-11-08 16:01:16 +0200

    automatic connection
    raspberry pi side code for automatic connection

--- comms.py
+++ comms.py
@@ -1,5 +1,13 @@
 import socket
-HOST = input("PC ip: ")
+import serial
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5)
+print("Waiting for IP...")
+while True:
+    if ser.in_waiting > 0:
+        ip = ser.readline().decode('utf-8').strip()
+        HOST = ip
+        break
+print(HOST)
 PORT = 65432
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
@@ -11,3 +19,11 @@ except KeyboardInterrupt:
 	print("shutdown")
 finally:
 	client_socket.close()
+
+
+
+
+# Adjust the port name based on your Pi’s device
+# e.g. /dev/ttyUSB0 or /dev/ttyACM0
+
+
